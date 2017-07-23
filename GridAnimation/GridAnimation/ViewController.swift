937eed15657b88a93621db55eec3701fed6bbc9f
Marcus <kaheiman@yahoo.com.hk>
2017-07-23 17:30:05 +0800

    magical grid

---
+++ GridAnimation/GridAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GridAnimation
+//
+//  Created by Marcus Man on 23/7/2017.
+//  Copyright © 2017 Marcus Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
